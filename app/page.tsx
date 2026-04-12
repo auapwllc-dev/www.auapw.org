fc6c1d457093b262f92417607d77e0855f81fe66
AUAPW LLC <266437609+auapwllc-dev@users.noreply.github.com>
2026-04-12 08:39:40 +0530

    Update app/page.tsx with new content.

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <div>
-      <Image src="/images/nextjs.svg" alt="Next.js" /> {/* Line 151 */}
-      <Image src="/images/nuxt.svg" alt="Nuxt.js" /> {/* Line 163 */}
-      <Image src="/images/svelte.svg" alt="Svelte" /> {/* Line 172 */}
+      <Image src="/images/nextjs.svg" alt="Next.js" width={200} height={200} />
+      <Image src="/images/nuxt.svg" alt="Nuxt.js" width={200} height={200} />
+      <Image src="/images/svelte.svg" alt="Svelte" width={200} height={200} />
     </div>
   );
 }
